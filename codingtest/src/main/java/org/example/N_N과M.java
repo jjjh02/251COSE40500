7366dd53d5c4239fa99711e1b4fe33d8b7466353
jjjh02 <jinnyshur@korea.ac.kr>
2025-06-15 23:36:34 +0900

    feat:: N과M 문제 설명 추가

--- codingtest/src/main/java/org/example/N_N과M.java
+++ codingtest/src/main/java/org/example/N_N과M.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+//
+//        1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+
 public class N_N과M {
+
 }
